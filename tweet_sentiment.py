d78749924cbba87d9ee717c2f526a092664ded5b
Morgan Visnesky <morganticss@gmail.com>
2018-01-11 22:19:37 -0500

    added linkgrabber and updated SQLite code to reflect

--- tweet_sentiment.py
+++ tweet_sentiment.py
@@ -72,7 +72,11 @@ def __main__():
     filename = 'twitter_sentiment_test.db'
     conn = sqlite3.connect(filename)
     cur = conn.cursor()
-    cur.execute('CREATE TABLE IF NOT EXISTS twitter_sentiment(twitter_handle text, tweet text, keyword text, sentiment text)')
+    cur.execute('''CREATE TABLE IF NOT EXISTS twitter_sentiment(twitter_handle text,
+                                                                tweet text,
+                                                                keyword text,
+                                                                sentiment text,
+                                                                link(s)_in_tweet text)''')
 
     keywords = get_user_keywords()
 
@@ -83,12 +87,24 @@ def __main__():
         except tweepy.TweepError as e:
             twitter_name = input("The handle you entered was invalid, please try again: ")
         catchcount = 0
+
     for status in stuff:
         for word in keywords:
             if word in status.text.lower():
+
+                # regex for link grabber
+                url = ''
+                links = re.search("(?P<url>https?://[^\s]+)", status.text)
+                if links:
+                    url = links.groups()
+                    url = url[0]
+
+
+
                 sent = get_sentiment(status.text)
-                cur.execute("INSERT INTO twitter_sentiment VALUES (?,?,?,?)", (twitter_name, status.text, word, sent))
+                cur.execute("INSERT INTO twitter_sentiment VALUES (?,?,?,?,?)", (twitter_name, status.text, word, sent, str(url)))
                 catchcount += 1
+
             else:
                 continue
 
