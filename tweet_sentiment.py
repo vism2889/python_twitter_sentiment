bc7f7e3d6eb628acd736a5d210dce462aa95c590
Morgan Visnesky <morganticss@gmail.com>
2018-01-14 16:49:19 -0500

    Added functionality to grab text from full tweet.
    - Added tweet_mode = 'extended' to the api.user_timeline call in addition
      to changing all the previous calls to status.text to status.full_text.
    
    - Changed header format of tweet_sentiment.py file to something more
      widely adopted as well as adding a free software GNU license.
    
    - Made alterations and additions to 'TODO' realted comments.

--- tweet_sentiment.py
+++ tweet_sentiment.py
@@ -1,14 +1,28 @@
-'''
-File Name: tweet_sentiment.py
-Author: Morgan Visnesky
-Date: 01/02/2018
+#!/usr/bin/env python
+"""
+Application to use sentiment analysis on user determined tweets then store
+resulting data in SQLite database.
 
-Description:
 
-Application to use sentiment analysis on user determined tweets then store resulting
-data in SQLite database.
+tweet_sentiment.py uses python in conjunction with the tweepy, and TextBlob
+packages for crawling tweets and perfroming sentiment analysis respectively.
+After inputing a comma seperated list of user defined keywords, and then
+entering a valid twitter handle tweet_sentiment.py will grab valid tweets
+correlating to the user defined keywords from the 100 most recent tweets of the
+selcted twitter handle.
 
-Planned Improvements:
+Minimal functionality is currently implemented to parse for links and future
+plans exist to have it follow those links and parse them as well, returning
+additional sentiment data, to possibly be averaged into the original sentiment
+in a meaningful way.
+
+All data returned from the search is stored in a SQLite database, the name of
+which is printed to the screen upon completion of the search along with the
+number of relevant tweets stored in the database.
+
+TODO:
+    ** add contextual semantic search
+    ** add intent analysis
 
     - tweet_cleaner() function, see example below
     (in_progress)- ext_link_parser() function to follow link, parse webpage
@@ -17,31 +31,49 @@ Planned Improvements:
     - break __main__ up into smaller reusable functions
 
     - make it so it you dont want to enter any keywords and just return all tweets from a given user you can
+      just grab thier last 100 tweets by default.
+    - display last used search terms in form of list (terms from last search session), allow selection of that
+      as opposed to entering in new list of terms or defaulting to no search terms.
+
+This file is part of python_twitter_sentiment.
+
+python_twitter_sentiment is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation, either version 3 of the License, or (at your option) any later
+version.
+python_twitter_sentiment is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+You should have received a copy of the GNU General Public License along with
+this program. If not, see <http://www.gnu.org/licenses/>.
+"""
+
+__author__ = "Morgan Visnesky"
+__authors__ = ["One developer", "And another one", "etc"]
+__contact__ = "morganticss@gmail.com"
+__date__ = "2018/01/02"
+__license__ = "GPLv3"
+__maintainer__ = "Morgan Visnesky"
+__status__ = "Developement"
+__version__ = "0.0.1"
 
 
-'''
 import datetime
 import re
 import sqlite3
 import tweepy
 import json
-#import sentiment
 from textblob import TextBlob as tb
 #import urllib.request
 #from bs4 import BeautifulSoup
-
-
-# make demo file showing how to store auth, and token in seperate file
 from morgstwtkey import auth, token
 auth #OAuth
 token #Access token
 api = tweepy.API(auth)
 
-
-
-
 #create a tweet cleaning function to pass tweets through before the get_sentiment function
 '''
+TODO:
 def tweet_cleaner(tweet):
     - make sure textblob removes stopwords, if not do so.
     - clean punctuation
@@ -50,9 +82,11 @@ def tweet_cleaner(tweet):
 '''
 
 '''
+TODO:
 def ext_link_parser(link):
     - go to link and parse (maybe use beautifulSoup)
-    - return sentiment to a large body of text anywhere in the link that key words from the user are found
+    - return sentiment to a large body of text anywhere in the link that key
+      words from the user are found
     -
 
 '''
@@ -81,44 +115,46 @@ def __main__():
                                                                 tweet text,
                                                                 keyword text,
                                                                 sentiment text,
-                                                                link(s)_in_tweet text)''')
+                                                                link_in_tweet text)''')
 
     keywords = get_user_keywords()
 
     twitter_name = input("Enter a Twitter Handle: ")
     for name in twitter_name:
         try:
-            stuff = api.user_timeline(screen_name = twitter_name, count = 100, include_rts = False)
-            # api.user_timeline currently doesnt return full tweet, returns partial with t.co link at the end
-            # created link grabber, but it seems the t.co links are unreasonably hard to parse for one
-            # tweets worth of data.
+            stuff = api.user_timeline(screen_name = twitter_name,
+                                    count = 100,
+                                    include_rts = False,
+                                    tweet_mode= 'extended')
+            # tweet_mode = 'extended'  in addition status.full_text accurately grabs
+            # entire tweet with t.co link at end
+
         except tweepy.TweepError as e:
             twitter_name = input("The handle you entered was invalid, please try again: ")
-        catchcount = 0
+        catchcount = 0 # used to keep track of num of tweets returned
 
     for status in stuff:
         for word in keywords:
-            if word in status.text.lower(): # lowercase as cleaning measure for proper matching
+            if word in status.full_text.lower(): # lowercase as cleaning measure for proper matching
 
                 # re.search + link grabber
-                # would eventually like to create another one specific for external weblinks
-                # and use this one for t.co links specifically unless can manage another way to return full tweets
-                # from tweepy
                 url = ''
-                links = re.search("(?P<url>https?://[^\s]+)", status.text)
+                links = re.search("(?P<url>https?://[^\s]+)", status.full_text)
                 if links:
                     url = links.groups()
                     url = url[0]
-
-
-
-                sent = get_sentiment(status.text)
-                cur.execute("INSERT INTO twitter_sentiment VALUES (?,?,?,?,?)", (twitter_name, status.text, word, sent, str(url)))
-                catchcount += 1
+                # status passed to sentiment function
+                sent = get_sentiment(status.full_text.lower())
+                # data inserted in to corresponding DB columns
+                cur.execute("INSERT INTO twitter_sentiment VALUES (?,?,?,?,?)", (twitter_name, status.full_text, word, sent, str(url)))
+                catchcount += 1 # if keyword is in tweet and has been collected add 1 to catchcount
 
             else:
                 continue
 
+    '''
+    TODO: Return the number of links stored from current search with list of attached links
+    '''
     print('\nThis search caught %d tweets' % catchcount)
     print('Data was added to: %s \n' % filename)
 
